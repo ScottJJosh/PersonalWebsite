be864f00fe7e547c9c8cc33a3149a2d45750fcd1
ScottJJosh <Scott.j.Joshua@gmail.com>
2024-05-10 21:58:05 -0500

    Fixed landing page and added a lot of stuff

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,10 +1,50 @@
 import React from "react";
 import './PageStyling/HomeStyles.css';
-
-
+import {ReactTyped} from 'react-typed';
+import ProjectComponent from "../Components/projectComponent";
+import selfPortrait from "../Images/IMG_2202.JPG";
 function Home() {
     return(
-        <div className="background">Welcome to the Home</div>
+        <div className="scroll-container">
+            <div className="welcomeContainer scroll-item">
+                <div className="typedComponent">
+                    <ReactTyped
+                        strings ={
+                            ["Welcome to my Website",
+                                "Click on the navbar to navigate",
+                                "This is a sick Library"
+                            ]
+                        }
+                        typeSpeed = {70}
+                        backSpeed = {60}
+                        loop/>
+                </div>
+                <div className="buttonContainer">
+                    <button className="welcomeButtons">
+                        Resume
+                    </button>
+                    <button className="welcomeButtons">
+                        Interests
+                    </button>
+                </div>
+            </div>
+            <div className="heroProjectContainer scroll-item">
+                <ProjectComponent Link="https://github.com/ScottJJosh" 
+                    ImageSource={selfPortrait} Title="Project 4"
+                    Description="Here is the project description a little excerpt to tell about the project"/>
+                <div className="heroProjectDescription">
+                    Here is my Latest project. Click to check it out!
+                </div>
+            </div>
+            <div className="heroInterestsContainer scroll-item">
+                <div>
+                    Check out my interests
+                </div>
+            </div>
+            <div className="Footer">
+                This is the working footer
+            </div>
+        </div>
     );
 }
 
